2aef8f0f9fb2bcd163529d19c1941a87338980c1
Herwig Bogaert <herwig.bogaert@viaa.be>
2017-04-24 21:57:18 +0200

    refactor tests

--- spec/swarmbucket_spec.rb
+++ spec/swarmbucket_spec.rb
@@ -2,15 +2,17 @@ require_relative '../swarmbucket'
 
 describe SwarmBucket do
     let (:swarmhttp) { SwarmBucket.new 'domain','bucket' }
-    let (:httpresponse) { double 'response' }
+    let (:httpsuccess) { Net::HTTPSuccess.new(1.1, '200', 'OK')}
+    let (:httpredirect) { Net::HTTPRedirection.new(1.1, '301', 'Moved Permanently')}
+    let (:httpnotfound) { Net::HTTPNotFound.new(1.1, '404', 'Not Found')}
+    let (:http) { double 'http'}
     let! (:httprequests) {[]}
 
-    # Mock the http connection returning httpresponse
+    # Mock the http redirection
     # and collect the request objects
     before :each do
-        http = double 'http'
-        allow(Net::HTTP).to receive(:start) { httpresponse }
-        .and_yield(http)
+        allow(httpredirect).to receive(:[])
+        .with('location') { 'http://newhost/newpath?param' }
         allow(http).to receive(:request) do |req|
             httprequests << req.dup
         end
@@ -22,7 +24,7 @@ describe SwarmBucket do
         end
         it 'opens a HTTP connection' do
             expect(Net::HTTP).to have_received(:start)
-            .with('domain',80)
+            .with('domain',80).once
         end
         it 'has the correct request method' do
             expect(httprequests.map &:method)
@@ -32,33 +34,31 @@ describe SwarmBucket do
         end
         it 'has the correct request path' do
             expect(httprequests.map &:path)
-            .to include '/bucket/objectname'
+            .to start_with '/bucket/objectname'
         end
         it 'returns the response object' do
-            expect(@response).to be(httpresponse)
+            expect(@response).to be(httpsuccess)
         end
     end
     shared_examples "http_redirected_request" do |method|
         context 'when not redirected' do
             before :each do
-                expect(Net::HTTPRedirection).to receive(:===)
-                .with(httpresponse) { false }
+                allow(Net::HTTP).to receive(:start).and_return(httpsuccess)
+                .and_yield(http)
             end
             include_examples 'http_request', method
         end
         context 'when redirected twice' do
             before :each do
-                expect(Net::HTTPRedirection).to receive(:===)
-                .with(httpresponse).and_return(true, true, false)
-                allow(httpresponse).to receive(:[])
-                .with('location') { 'http://newhost/newpath?param' }
+                allow(Net::HTTP).to receive(:start).and_return(httpredirect,httpredirect,httpsuccess)
+                .and_yield(http)
             end
             include_examples 'http_request', method
             it 'follows the redirection' do
                 expect(Net::HTTP).to have_received(:start)
                 .with('newhost',80).twice
                 expect(httprequests.map &:path)
-                .to end_with('/newpath?param','/newpath?param' )
+                .to eq ['/bucket/objectname','/newpath?param','/newpath?param']
             end
         end
     end
@@ -84,10 +84,8 @@ describe SwarmBucket do
     end
     context 'lifepoints' do
         before :each do
-            expect(Net::HTTPRedirection).to receive(:===)
-            .with(httpresponse).and_return(true, false)
-            allow(httpresponse).to receive(:[])
-            .with('location') { 'http://newhost/newpath?param' }
+            allow(Net::HTTP).to receive(:start).and_return(httpredirect,httpsuccess)
+                .and_yield(http)
             # Set current time during test at 2016-06-29 11:30:39 +0200
             allow(Time).to receive(:now) { Time.at 1467192639 }
         end
@@ -118,31 +116,29 @@ describe SwarmBucket do
             end
         end
         context '#present?' do
-            before :each do
-            end
             subject { swarmhttp.present? 'objectname' }
             context 'when the object does not exist' do
                 before :each do
-                    expect(Net::HTTPSuccess).to receive(:===)
-                    .with(httpresponse).and_return(false)
+                    allow(Net::HTTP).to receive(:start).and_return(httpnotfound)
+                    .and_yield(http)
                 end
                 it { is_expected.to be false }
             end
             context 'when the object exists' do
                 before :each do
-                    expect(Net::HTTPSuccess).to receive(:===)
-                    .with(httpresponse).and_return(true)
+                    allow(Net::HTTP).to receive(:start).and_return(httpredirect,httpsuccess)
+                    .and_yield(http)
                 end
                 context 'without a lifepoint' do
                     before :each do
-                        allow(httpresponse).to receive(:[])
+                        allow(httpsuccess).to receive(:[])
                         .with('lifepoint') { nil }
                     end
                     it { is_expected.to be true }
                 end
                 context 'with a delete lifepoint' do
                     before :each do
-                        allow(httpresponse).to receive(:[])
+                        allow(httpsuccess).to receive(:[])
                         .with('lifepoint')
                         .and_return "[Wed, 29 Jun 2016 13:30:39 GMT] reps:16:4 deletable=True, [] delete"
                     end
@@ -150,7 +146,7 @@ describe SwarmBucket do
                 end
                 context 'with a non-delete lifepoint' do
                     before :each do
-                        allow(httpresponse).to receive(:[])
+                        allow(httpsuccess).to receive(:[])
                         .with('lifepoint')
                         .and_return "[Wed, 29 Jun 2016 13:30:39 GMT] reps:16:4 deletable=True, [] reps:2"
                     end
@@ -158,7 +154,7 @@ describe SwarmBucket do
                 end
                 context 'with mutliple lifepoints' do
                     before :each do
-                        allow(httpresponse).to receive(:[])
+                        allow(httpsuccess).to receive(:[])
                         .with('lifepoint') {[
                             "[Wed, 29 Jun 2016 12:30:39 GMT] reps:16:4 deletable=False",
                             "[Wed, 29 Jun 2016 13:30:39 GMT] reps:2 deletable=True",
